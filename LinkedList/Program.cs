a26b2a7c91a74d1f5baa0848136df8a5a2a4ddd6
Mohamed_Fayyad <mohamedfayad080@gmail.com>
2023-09-20 03:49:31 +0300

    add taskes and node manually

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,6 +4,18 @@
     {
         static void Main(string[] args)
         {
+            Task task1 = new(1, "study DS");
+            Task task2 = new(2, "workout for hour");
+            Task task3 = new(3, "study problem solving");
+
+            Node head = new(task1);
+            Node node2 = new(task2);
+            Node node3 = new(task3);
+
+            head.NextNode = node2;
+            node2.NextNode = node3;
+
+            Console.WriteLine(head.NextNode?.NextNode?.Task.Text);
 
         }
     }
