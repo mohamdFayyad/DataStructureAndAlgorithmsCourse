139f36faec045e7d49a45c4b0605b93eb5a2e65b
Mohamed_Fayyad <mohamedfayad080@gmail.com>
2023-09-20 03:26:11 +0300

    Add project files.

---
+++ DataStructureAndAlgorithmes/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DataStructureAndAlgorithmes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Please enter the number you want to guess: ");
+                int number = int.Parse(Console.ReadLine());
+
+                //Console.Write("Please enter a list of numbers: ");
+                //// Split the input string into an array of strings based on spaces
+                ////string[] numberStrings = input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                //string[] numberStrings = Console.ReadLine().Split(' ');
+
+
+                //// Convert the string array to a list of integers
+                //List<int> numbers = numberStrings.Select(int.Parse).ToList();
+
+                Console.WriteLine($"index of number is {FindNumberInList(new List<int> { 20, 18, 16, 10, 8, 6, 4, 2, 1 }, number)}");
+
+                Console.WriteLine("\n###################################\n");
+            }
+        }
+        public static int FindNumberInList(List<int> listNumbers, int number)
+        {
+            int loweIndex = 0;
+            int higheIndex = listNumbers.Count - 1;
+            while (higheIndex >= loweIndex)
+            {
+                int midIndex = (loweIndex + higheIndex) / 2;
+
+                if (listNumbers[midIndex] == number)
+                    return midIndex + 1;
+
+                if (listNumbers[midIndex] > number)
+                    loweIndex = midIndex + 1;
+
+                else
+                    higheIndex = midIndex - 1;
+
+            }
+            return -1;
+        }
+    }
+}
---
+++ LinkedList/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Task
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public Task(int id, string text)
+        {
+            Id = id;
+            Text = text;
+        }
+    }
+    
+    internal class Node
+    {
+        public Task Task { get; set; }
+        public Node? NextNode { get; set; }
+        public Node(Task task)
+        {
+            Task = task;
+            NextNode = null;
+        }
+    }
+}
